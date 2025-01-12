62ea843525c8028077b3de7c019e109e82575879
Your Name <you@example.com>
2025-01-12 16:07:01 +0000

    Fix jsonl bug

--- cai/core.py
+++ cai/core.py
@@ -81,8 +81,7 @@ class CAI:
             debug,
             "Getting chat completion for...:",
             messages,
-            brief=self.brief,
-        )
+            brief=self.brief)
 
         tools = [function_to_json(f) for f in agent.functions]
         # hide context_variables from model
@@ -153,7 +152,7 @@ class CAI:
                     return Result(value=str(result))
                 except Exception as e:
                     error_message = f"Failed to cast response to string: {result}. Make sure agent functions return a string or Result object. Error: {str(e)}"  # noqa: E501 # pylint: disable=C0301
-                    debug_print(debug, error_message, brief=self.brief, )
+                    debug_print(debug, error_message, brief=self.brief)
                     raise TypeError(error_message) from e
 
     def handle_tool_calls(
@@ -207,8 +206,7 @@ class CAI:
                 debug_print(
                     debug,
                     f"Tool {name} not found in function map.",
-                    brief=self.brief,
-                )
+                    brief=self.brief)
                 partial_response.messages.append(
                     {
                         "role": "tool",
@@ -225,8 +223,7 @@ class CAI:
                 name,
                 "with arguments",
                 args,
-                brief=self.brief,
-            )
+                brief=self.brief)
 
             func = function_map[name]
             # pass context_variables to agent functions
@@ -317,12 +314,11 @@ class CAI:
                 debug,
                 "Received completion:",
                 message,
-                brief=self.brief,
-            )
+                brief=self.brief)
             history.append(message)
 
             if not message["tool_calls"] or not execute_tools:
-                debug_print(debug, "Ending turn.", brief=self.brief, )
+                debug_print(debug, "Ending turn.", brief=self.brief)
                 break
 
             # convert tool_calls to objects
@@ -409,8 +405,7 @@ class CAI:
                 debug,
                 "Received completion:",
                 message,
-                brief=self.brief,
-            )
+                brief=self.brief)
             message.sender = active_agent.name
             history.append(
                 json.loads(message.model_dump_json())
