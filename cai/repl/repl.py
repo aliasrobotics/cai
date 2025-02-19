ae2239ed225be007252aa58b4f050fefcda4b2e2
Mery-Sanz <maria@aliasrobotics.com>
2025-02-19 08:10:07 +0000

    solve cli.py

--- cai/repl/repl.py
+++ cai/repl/repl.py
@@ -175,26 +175,30 @@ def run_demo_loop(  # pylint: disable=too-many-locals,too-many-nested-blocks,too
             if response.agent:
                 agent = response.agent
         except KeyboardInterrupt:
-            if os.getenv("CAI_REPORT", "false").lower() == "true":
-                if os.getenv("CTF_NAME"):
-                    from extensions.report.ctf.ctf_reporter_agent import reporter_agent  # pylint: disable=import-error # noqa: E501
-                    template = "extensions/report/ctf/template.md"
-                else:
+            if os.getenv("CAI_REPORT"):
+                if os.getenv("CAI_REPORT", "ctf").lower() == "pentesting":
                     from extensions.report.pentesting.pentesting_agent import reporter_agent  # pylint: disable=import-error # noqa: E501
                     template = "extensions/report/pentesting/template.md"
-            client = CAI(state_agent=state_agent, force_until_flag=False)
-            response_report = client.run(
-                agent=reporter_agent,
-                messages=[{"role": "user", "content": "Do a report from " +
-                           "\n".join(
-                               msg['content'] for msg in response.messages
-                               if msg.get('content') is not None
-                           )}],
-                debug=float(os.getenv('CAI_DEBUG', '2')),
-                max_turns=float(os.getenv('CAI_MAX_TURNS', 'inf')),
-            )
+                elif os.getenv("CAI_REPORT", "ctf").lower() == "nis2":
+                    from extensions.report.nis2.nis2_report_agent import reporter_agent  # pylint: disable=import-error # noqa: E501
+                    template = "extensions/report/nis2/template.md"
+                else:
+                    from extensions.report.ctf.ctf_reporter_agent import reporter_agent  # pylint: disable=import-error # noqa: E501
+                    template = "extensions/report/ctf/template.md"
+            
+                client = CAI(state_agent=state_agent, force_until_flag=False)
+                response_report = client.run(
+                    agent=reporter_agent,
+                    messages=[{"role": "user", "content": "Do a report from " +
+                            "\n".join(
+                                msg['content'] for msg in response.messages
+                                if msg.get('content') is not None
+                            )}],
+                    debug=float(os.getenv('CAI_DEBUG', '2')),
+                    max_turns=float(os.getenv('CAI_MAX_TURNS', 'inf')),
+                )
 
-            report_data = json.loads(response_report.messages[0]['content'])
-            report_data["history"] = json.dumps(response.messages, indent=4)
-            create_report(report_data, template)
+                report_data = json.loads(response_report.messages[0]['content'])
+                report_data["history"] = json.dumps(response.messages, indent=4)
+                create_report(report_data, template)
             break
