9fdf974e6e4dd09d557c2dd15887a4d2e77b15e4
Francesco Balassone <77972200+duel0@users.noreply.github.com>
2025-04-04 10:24:26 +0200

    Added DFIR agent

---
+++ cai/agents/dfir.py
@@ -0,0 +1,46 @@
+"""DFIR Base Agent
+SSH_PASS
+SSH_HOST
+SSH_USER
+"""
+import os
+from cai.types import Agent  # pylint: disable=import-error
+from cai.util import load_prompt_template  # Add this import
+from cai.tools.command_and_control.sshpass import (  # pylint: disable=import-error # noqa: E501
+    run_ssh_command_with_credentials
+)
+
+from cai.tools.reconnaissance.generic_linux_command import (  # pylint: disable=import-error # noqa: E501
+    generic_linux_command
+)
+
+from cai.tools.reconnaissance.exec_code import (  # pylint: disable=import-error # noqa: E501
+    execute_code
+)
+from cai.tools.web.search_web import (  # pylint: disable=import-error # noqa: E501
+    make_web_search_with_explanation,
+)
+# Prompts
+dfir_agent_system_prompt = load_prompt_template("prompts/system_dfir_agent.md")
+# Define functions list based on available API keys
+functions = [
+    generic_linux_command,
+    run_ssh_command_with_credentials,
+    execute_code,
+]
+
+if os.getenv('PERPLEXITY_API_KEY'):
+    functions.append(make_web_search_with_explanation)
+
+dfir_agent = Agent(
+    name="DFIR Agent",
+    instructions=dfir_agent_system_prompt,
+    description="""Agent that specializes in Digital Forensics and Incident Response.
+                   Expert in investigation and analysis of digital evidence.""",
+    model=os.getenv('CAI_MODEL', "qwen2.5:14b"),
+    functions=functions,
+    parallel_tool_calls=False,
+)
+
+def transfer_to_dfir_agent():
+    return dfir_agent
